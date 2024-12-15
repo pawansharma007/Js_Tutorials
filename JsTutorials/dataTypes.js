7eada5aadab0960eefb1e60bcccc08f4cf06e224
pawansharma007 <31450140+pawansharma007@users.noreply.github.com>
2024-12-15 09:57:44 +0000

    datatypes

---
+++ JsTutorials/dataTypes.js
@@ -0,0 +1,38 @@
+"use strict" 
+/*treats all javascript code as newer version. 
+However, latest js files are by default using new version which contains classes, objects etc */
+
+// alert( 3 + 3) 
+/*this will throw error as we are running js through node now . 
+When run through browser this will give a pop up with value 6*/
+
+//Below code will print 6 but readabiltiy si not good if written like this
+console.log( 3
+    +
+    3
+) 
+console.log("Pawan");
+
+let name= "Pawan";
+let age= 30;
+let isLoggedIn = false
+
+/*Primitive Data Types 
+number -> 2 to 2 to power 53
+bigint
+String -> write either in "" or ''
+boolean -> true/false
+null -> standalone value
+undefined -> undefined which is not defined and doesnot means it si null
+symbol-> used to find uniquesness during figna or react codes */
+
+/* Non Primitve Data Types
+Object
+null
+undefined */
+
+console.log(typeof(null));    // it is of object type
+console.log(typeof(undefined)); //it is of undefined type
+
+
+
